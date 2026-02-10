4be30e3aed0f7c2bdf796c16109747422f326337
Ruslan Bochin <ruslan.bochin071@gmail.com>
2026-02-10 12:43:31 +0300

    ruslan_bochin_homework_24 memes fix

--- test_api_memes/endpoints/create_meme.py
+++ test_api_memes/endpoints/create_meme.py
@@ -11,20 +11,10 @@ class CreateMeme(Endpoint):
     def create_meme(self, payload, headers=None):
         headers = headers or self.headers
         self.response = requests.post(
-            f"http://memesapi.course.qa-practice.com/meme",
+            "http://memesapi.course.qa-practice.com/meme",
             json=payload,
-            headers=headers
+            headers=headers,
         )
         self.json = self.response.json()
         self.meme_id = self.json["id"]
         return self.response
-
-    @allure.step("Create meme 2")
-    def create_meme_text(self, text, url, tags, author, headers=None):
-        payload = {
-            "text": text,
-            "url": url,
-            "tags": tags,
-            "info": {"author": author}
-        }
-        return self.create_meme(payload)
